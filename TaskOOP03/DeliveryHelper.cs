fc41ca7b7b5d871efc03e0653009a3812d30a02b
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-18 17:52:04 +0300

    DeliveryHelper

---
+++ TaskOOP03/DeliveryHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskOOP03
+{
+    internal class DeliveryHelper
+    {
+        public static void PrintShipmentDetails(
+    Shipment shipment)
+        {
+            shipment.PrintShipment();
+        }
+    }
+}
