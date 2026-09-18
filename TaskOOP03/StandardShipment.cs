ef5b99d58b05693738f1a994253b293943bf850a
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-18 17:47:37 +0300

    StandardShipment

---
+++ TaskOOP03/StandardShipment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskOOP03
+{
+    internal class StandardShipment : Shipment
+    {
+        public StandardShipment(
+           string trackingCode,
+           string description,
+           double weight,
+           double deliveryFee,
+           string city,
+           string street)
+           : base(
+               trackingCode,
+               description,
+               weight,
+               deliveryFee,
+               city,
+               street)
+        {
+        }
+
+        public override void PrintShipment()
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Standard Shipment");
+            Console.WriteLine("---------------------------------------------");
+
+            base.PrintShipment();
+
+            Console.WriteLine(
+                "Estimated Cost: " + EstimatedCost + " EGP");
+        }
+    }
+}
