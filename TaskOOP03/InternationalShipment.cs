785a85a13764983a5502ce67e267dff85228d813
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-18 17:49:20 +0300

    InternationalShipment

---
+++ TaskOOP03/InternationalShipment.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskOOP03
+{
+    internal class InternationalShipment : Shipment
+    {
+        public string DestinationCountry { get; set; }
+        public double CustomsFee { get; set; }
+
+        public InternationalShipment(
+            string trackingCode,
+            string description,
+            double weight,
+            double deliveryFee,
+            string destinationCountry,
+            double customsFee,
+            string city,
+            string street)
+            : base(
+                trackingCode,
+                description,
+                weight,
+                deliveryFee,
+                city,
+                street)
+        {
+            DestinationCountry = destinationCountry;
+            CustomsFee = customsFee;
+        }
+
+        public override double EstimatedCost
+        {
+            get
+            {
+                return DeliveryFee + (Weight * 5) + CustomsFee;
+            }
+        }
+
+        public override void PrintShipment()
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("International Shipment");
+            Console.WriteLine("---------------------------------------------");
+
+            base.PrintShipment();
+
+            Console.WriteLine(
+                "Destination Country : " + DestinationCountry);
+
+            Console.WriteLine(
+                "Customs Fee         : " + CustomsFee + " EGP");
+
+            Console.WriteLine(
+                "Estimated Cost      : " + EstimatedCost + " EGP");
+        }
+
+        public virtual void GenerateCustomsReport()
+        {
+            Console.WriteLine(
+                "[Customs Report] Standard customs report for "
+                + TrackingCode);
+        }
+
+    }
+}
