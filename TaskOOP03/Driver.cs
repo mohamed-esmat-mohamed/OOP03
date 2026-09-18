d9b7db8a6bb00008b7ac771cccd096f967e31c4d
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-18 17:45:35 +0300

    Driver

---
+++ TaskOOP03/Driver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskOOP03
+{
+    internal class Driver
+    {
+        public string DriverId { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Driver(string driverId, string fullName, string phoneNumber)
+        {
+            DriverId = driverId;
+            FullName = fullName;
+        }
+    }
+}
