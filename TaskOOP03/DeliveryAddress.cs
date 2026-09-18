bd3bcf89872dea234baa4d1d194d2662b8f2ebc3
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-18 17:44:51 +0300

    DelivryAdd

---
+++ TaskOOP03/DeliveryAddress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskOOP03
+{
+    internal class DeliveryAddress
+    {
+        public string City { get; private set; }
+        public string Street { get; private set; }
+
+        public DeliveryAddress(string city, string street)
+        {
+            City = city;
+            Street = street;
+        }
+    }
+}
