04d7728b21e00fd72d96df988ff3146fac7ee786
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-18 17:50:09 +0300

    PriorityInternationalShipment

---
+++ TaskOOP03/PriorityInternationalShipment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskOOP03
+{
+    internal class PriorityInternationalShipment : InternationalShipment
+    {
+        public PriorityInternationalShipment(
+        string trackingCode,
+        string description,
+        double weight,
+        double deliveryFee,
+        string destinationCountry,
+        double customsFee,
+        string city,
+        string street)
+        : base(
+            trackingCode,
+            description,
+            weight,
+            deliveryFee,
+            destinationCountry,
+            customsFee,
+            city,
+            street)
+        {
+        }
+
+        public sealed override void GenerateCustomsReport()
+        {
+            Console.WriteLine(
+                "[Priority Customs Report] Fast-tracked customs clearance for "
+                + TrackingCode);
+        }
+    }
+}
